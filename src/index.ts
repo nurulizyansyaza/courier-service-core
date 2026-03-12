172181a52adb34e7a6c6a6533839571790df6d2b
Yang Edruce <nurulizyansyaza@gmail.com>
2026-03-13 03:33:26 +0800

    fix minWeight to weight

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export { parsePackages, parseFleet } from './InputValidator';
 import { Offer } from './types';
 
 export const DEFAULT_OFFERS: Offer[] = [
-  { code: 'OFR001', discount: 10, minWeight: 70, maxWeight: 200, minDistance: 0, maxDistance: 200, exclusiveMax: true },
-  { code: 'OFR002', discount: 7, minWeight: 100, maxWeight: 250, minDistance: 50, maxDistance: 150 },
-  { code: 'OFR003', discount: 5, minWeight: 10, maxWeight: 150, minDistance: 50, maxDistance: 250 },
+  { code: 'OFR001', discount: 10, weight: { min: 70, max: 200 }, distance: { min: 0, max: 200, exclusive: true } },
+  { code: 'OFR002', discount: 7, weight: { min: 100, max: 250 }, distance: { min: 50, max: 150 } },
+  { code: 'OFR003', discount: 5, weight: { min: 10, max: 150 }, distance: { min: 50, max: 250 } },
 ];
